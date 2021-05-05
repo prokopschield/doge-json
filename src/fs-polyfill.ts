123adce47c6f5a6ca6445480f01c5814ea60309a
Prokop <prokop.schield@protonmail.com>
2021-05-05 15:20:59 +0200

    1.0.3 Fix mkdir in fs-polyfill

--- src/fs-polyfill.ts
+++ src/fs-polyfill.ts
@@ -9,7 +9,7 @@ if ((typeof window === 'object') && (window.localStorage)) {
 			existsSync: (path: string) => `doge-json_${path}` in window.localStorage,
 			readFileSync: (path: string) => window.localStorage.getItem(`doge-json_${path}`),
 			readdirSync: (path: string) => Object.keys(window.localStorage).filter(a => a.includes(path)).map(a => a.substr(a.indexOf(path) + path.length)),
-			mkdirSync: (path: string) => window.localStorage.setItem(path, JSON.stringify(fs.readdirSync(path))),
+			mkdirSync: (path: string) => window.localStorage.setItem(`doge-json_${path}`, JSON.stringify(fs.readdirSync(path))),
 			statSync: (path: string) => ({ isFile: () => !!window.localStorage.getItem(path), isDirectory: () => !window.localStorage.getItem(path) }),
 		});
 	}

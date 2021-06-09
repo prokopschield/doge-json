b766d53072d58ca776b27446933f999f10804f44
Prokop Schield <prokop.schield@protonmail.com>
2021-06-09 23:03:58 +0200

    1.0.4 Create directory when writing file

--- src/read.ts
+++ src/read.ts
@@ -6,27 +6,39 @@ import {
 } from 'path';
 
 export function read (file: string): any {
-	if (!fs.existsSync(file)) {
-		const dir = resolve(file, '..');
-		const base = basename(file);
-		const rd = fs.readdirSync(dir)
-		.filter(a => a.includes(base))
-		if (rd.length) {
-			for (const filename of rd) {
-				const tfile = resolve(dir, filename);
-				const stat = fs.statSync(tfile);
-				if (!stat.isDirectory()) {
-					file = tfile;
-					break;
+	try {
+		if (!fs.existsSync(file)) {
+			const dir = resolve(file, '..');
+			const base = basename(file);
+			const rd = fs.readdirSync(dir)
+			.filter(a => a.includes(base))
+			if (rd.length) {
+				for (const filename of rd) {
+					const tfile = resolve(dir, filename);
+					const stat = fs.statSync(tfile);
+					if (!stat.isDirectory()) {
+						file = tfile;
+						break;
+					}
 				}
 			}
 		}
+		if (fs.existsSync(file)) {
+			var data = fs.readFileSync(file, 'utf-8');
+			try {
+				data = JSON.parse(data);
+			} catch (e) {}
+			return data;
+		} else return null;
+	} catch (error) {
+		return null;
 	}
-	if (fs.existsSync(file)) {
-		var data = fs.readFileSync(file, 'utf-8');
-		try {
-			data = JSON.parse(data);
-		} catch (e) {}
-		return data;
-	} else return null;
 }
+
+export default read;
+module.exports = read;
+
+Object.assign(read, {
+	default: read,
+	read,
+});
--- src/write.ts
+++ src/write.ts
@@ -1,6 +1,17 @@
 import fs from './fs-polyfill';
+import path from 'path';
 
 export function write (file: string, data: any) {
 	if (!file.includes('.')) file = `${file}.json`;
+	const directory = path.resolve(file, '..');
+	if (!fs.existsSync(directory)) fs.mkdirSync(directory, { recursive: true });
 	fs.writeFileSync(file, JSON.stringify(data, null, '\t') + '\n');
 }
+
+export default write;
+module.exports = write;
+
+Object.assign(write, {
+	default: write,
+	write,
+});

d6764f4418cb3ce6b5dd4f54fd65f8f42a3de789
Prokop <prokop.schield@protonmail.com>
2024-10-31 13:00:08 +0100

    fix(src/fs-polyfill.ts): remove problematic typing of localStorage

--- src/fs-polyfill.ts
+++ src/fs-polyfill.ts
@@ -1,3 +1,5 @@
+export const localStorage = (globalThis as any)?.localStorage;
+
 export const fs: {
 	writeFileSync: (path: string, data: string) => void;
 	existsSync: (path: string) => boolean;
@@ -8,30 +10,27 @@ export const fs: {
 		isFile: () => boolean;
 		isDirectory: () => boolean;
 	};
-} =
-	typeof globalThis === 'undefined' ||
-	typeof globalThis.localStorage === 'undefined'
-		? require('fs')
-		: {
-				writeFileSync: (path: string, data: string) => {
-					globalThis.localStorage.setItem(`doge-json_${path}`, data);
-				},
-				existsSync: (path: string) =>
-					`doge-json_${path}` in globalThis.localStorage,
-				readFileSync: (path: string) =>
-					globalThis.localStorage.getItem(`doge-json_${path}`) || '',
-				readdirSync: (path: string) =>
-					Object.keys(globalThis.localStorage)
-						.filter((a) => a.includes(path))
-						.map((a) => a.slice(a.indexOf(path) + path.length)),
-				mkdirSync: (path: string) =>
-					globalThis.localStorage.setItem(
-						`doge-json_${path}`,
-						JSON.stringify(fs.readdirSync(path))
-					),
-				statSync: (path: string) => ({
-					isFile: () => !!globalThis.localStorage.getItem(path),
-					isDirectory: () => !globalThis.localStorage.getItem(path),
-				}),
-		  };
+} = !localStorage
+	? require('fs')
+	: {
+			writeFileSync: (path: string, data: string) => {
+				localStorage.setItem(`doge-json_${path}`, data);
+			},
+			existsSync: (path: string) => `doge-json_${path}` in localStorage,
+			readFileSync: (path: string) =>
+				localStorage.getItem(`doge-json_${path}`) || '',
+			readdirSync: (path: string) =>
+				Object.keys(localStorage)
+					.filter((a) => a.includes(path))
+					.map((a) => a.slice(a.indexOf(path) + path.length)),
+			mkdirSync: (path: string) =>
+				localStorage.setItem(
+					`doge-json_${path}`,
+					JSON.stringify(fs.readdirSync(path))
+				),
+			statSync: (path: string) => ({
+				isFile: () => !!localStorage.getItem(path),
+				isDirectory: () => !localStorage.getItem(path),
+			}),
+	  };
 export default fs;
